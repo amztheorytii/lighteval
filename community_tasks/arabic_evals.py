c69f524deffe6dfeb5d20da75e41e1448bed06b0
amztheorytii <ahmed.alzubaidi@t01pdscgpu18.ib0.cm.local>
2025-09-08 10:37:07 +0400

    add arabic_gsm8k

--- community_tasks/arabic_evals.py
+++ community_tasks/arabic_evals.py
@@ -1088,6 +1088,29 @@ alrage_qa_task = LightevalTaskConfig(
     version=0
 )
 
+def gsm8k_pfn(line, task_name: str = None):
+    # Has special analysis in metric for number decomposition
+    return Doc(
+        task_name=task_name,
+        query=f"السؤال: {line['question']}\nالجواب:",
+        choices=[f" {line['answer']}"],
+        gold_index=0,
+    )
+    
+GSM8K = LightevalTaskConfig(
+    name="gsm8k_arabic",
+    prompt_function=gsm8k_pfn,
+    suite=["community"],
+    hf_repo="Omartificial-Intelligence-Space/Arabic-gsm8k",
+    hf_subset="",
+    hf_avail_splits=["main_test","main_train"],
+    evaluation_splits=["main_test"],
+    few_shots_split=["main_train"],
+    metric=[Metrics.quasi_exact_match_gsm8k],
+    generation_size=1048,
+    stop_sequence=None,
+)
+
 
 TASKS_TABLE = (
     ARABIC_MMLU_TASKS
@@ -1110,4 +1133,5 @@ TASKS_TABLE = (
     + [toxigen_ar_task]
     + [sciq_ar_task]
     + [alrage_qa_task]
+    + [GSM8K]
 )
